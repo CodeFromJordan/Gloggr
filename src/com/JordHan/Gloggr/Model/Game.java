dd1246335885d35045ea109accd42ff16f67b3db
Jordan <jordan@jordan-crunchbang>
2014-05-09 20:41:01 +0100

    Added DatabaseHelper and DatabaseManager classes, made them ready to handle Game objects.

--- src/com/JordHan/Gloggr/Model/Game.java
+++ src/com/JordHan/Gloggr/Model/Game.java
@@ -1,29 +1,65 @@
 package com.JordHan.Gloggr.Model;
 
+import org.json.JSONObject;
+
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 @DatabaseTable
 public class Game {
     // Members and database field annotations
+	// Game identification information
     @DatabaseField(id = true)
-    private String id;
+    private String id;  
+    private String title;
+    private String system;
+    // User specific information
+    private int percentageFinished;
+    private int rating;
+    private String notes;
     
     // No argument constructor
     // Used for ORMLite
     public Game() { }
     
     // Constructor which uses JSONObject
+    public Game(JSONObject game) {
+    	// Parse JSON here
+    }
     
     // Constructor which uses separate parameters
-    public Game(String id) {
+    public Game(String id, String title, String system, 
+    		int percentageFinished, int rating, String notes) {
     	this.id = id;
+    	this.title = title;
+    	this.system = system;
+    	this.percentageFinished = percentageFinished;
+    	this.rating = rating;
+    	this.notes = notes;
     }
     
     // Getters
-    public String getID(JSONObject game) {
+    public String getID() {
         return this.id;
+    }
+    
+    public String getTitle() {
+    	return this.title;
+    }
+    
+    public String getSystem() {
+    	return this.system;
+    }
+    
+    public int getPercentageFinished() {
+    	return this.percentageFinished;
+    }
+    
+    public int getRating() {
+    	return this.rating;
+    }
+    
+    public String getNotes() {
+    	return this.notes;
     }
 }
---
+++ src/com/JordHan/db/DatabaseManager.java
@@ -0,0 +1,67 @@
+// Author: Jordan Hancock
+// Name: DatabaseManager.java
+// Last Modified: 18/02/2014
+// Purpose: Singleton class used for database access.
+package com.JordHan.db;
+
+import android.content.Context;
+import android.database.SQLException;
+import java.util.List;
+import com.JordHan.Gloggr.Model.Game;
+
+public class DatabaseManager {
+
+    static private DatabaseManager instance;
+
+    // Singleton
+    static public void init(Context ctx) {
+        if (instance == null) {
+            instance = new DatabaseManager(ctx);
+        }
+    }
+
+    static public DatabaseManager getInstance() {
+        return instance;
+    }
+
+    private DatabaseHelper helper;
+
+    private DatabaseManager(Context ctx) {
+        helper = new DatabaseHelper(ctx);
+    }
+
+    private DatabaseHelper getHelper() {
+        return this.helper;
+    }
+
+    // Get all games from database
+    public List<Game> getAllgames() throws SQLException {
+        List<Game> games = null;
+
+        try {
+            games = getHelper().getGameDao().queryForAll();
+        } catch (java.sql.SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return games;
+    }
+
+    // Add new game to database
+    public void addNewGame(Game newGame) {
+        try {
+            getHelper().getGameDao().create(newGame);
+        } catch (java.sql.SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    // Delete game from database
+    public void deleteGame(Game game) {
+        try {
+            getHelper().getGameDao().delete(game);
+        } catch (java.sql.SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
