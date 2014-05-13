f26464ed99f6bee5d5e07af688640bdedf03e00a
Jordan <jordan.hancock.92@gmail.com>
2014-05-13 12:31:56 +0100

    Made image on Home activity clickable so that it can be changed using a feature to implement later. Also redesigned app icons.

--- src/com/JordHan/Gloggr/MainActivity.java
+++ src/com/JordHan/Gloggr/MainActivity.java
@@ -5,6 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.JordHan.Gloggr.Helper.MenuHelper;
 import com.JordHan.Gloggr.db.DatabaseManager;
@@ -13,9 +17,20 @@ public class MainActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
-		DatabaseManager.init(this);
+		super.onCreate(savedInstanceState); // Push instance of activity to super
+		
+		// Setup interface
+		setContentView(R.layout.main_activity); // Load interface
+		ImageView imgProfilePicture = (ImageView)findViewById(R.id.imgProfilePicture);
+		
+		imgProfilePicture.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(getApplicationContext(), "Image clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		DatabaseManager.init(this); // Create instance of DatabaseManager ready for ORM
 	}
 	
     // When Menu button clicked
